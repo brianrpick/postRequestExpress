e27fcffcf364eed3ac63da9ecc3d8da830c267de
Brian Pick <brianr.pick@gmail.com>
2018-01-01 15:02:38 -0800

    added post route, and first redirect on app

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 var express = require("express");
 var app = express();
-
+var bodyParser = require("body-parser");
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
+var friends = ["tony", "jabecka", "jar jar", "Hessica", "Heffica"];
 
 app.get("/", function(req, res){
 	res.render("home");
 });
 
 app.get("/friends", function(req, res){
-	var friends = ["tony", "jabecka", "jar jar", "Hessica", "Heffica"];
 	res.render("friends", {friends: friends});
 })
 
 
-
+app.post("/addfriend", function(req, res) {
+	var newfriend = req.body.newfriend
+	friends.push(newfriend);
+	res.redirect("friends");
+})
 
 app.listen(3000, function() {
 	console.log("Serving app on port 3000");
